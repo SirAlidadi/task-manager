843b1688b4dfbcf59d004029c81024fdedba01d5
hamidrezasafayee <hamidrezasfy@gmail.com>
2022-07-11 16:38:45 +0430

    Build backend layout

---
+++ backend/app/core/Database.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Core;
+
+class Database
+{
+    public function __construct()
+    {
+        echo "Database ...";
+    }
+}
---
+++ backend/app/libs/TaskLib.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Libs;
+
+class TaskLib
+{
+    public function __construct()
+    {
+        // code
+    }
+}
